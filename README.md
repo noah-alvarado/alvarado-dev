006ee98813d03fa6552056cc58f5f318210f6e63
Noah Alvarado <noah@alvarado.dev>
2021-07-15 00:25:17 -0500

    now with firebase

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # alvarado.dev
+
+Run the app: `yarn dev` or `yarn start`
+
+Lint the app: `yarn lint` or `yarn lint:fix`
+
+Build the app: `yarn build`
+
+Build and deploy the app (uses Firebase): `yarn deploy`
--- src/firebase.ts
+++ src/firebase.ts
@@ -5,7 +5,13 @@ import { initializeApp } from 'firebase/app';
 // import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  // ...
+  apiKey: 'AIzaSyCmOaXfaZWZUxWjkFtHk2cuFTeuSdzEs_c',
+  authDomain: 'alvarado-dev.firebaseapp.com',
+  projectId: 'alvarado-dev',
+  storageBucket: 'alvarado-dev.appspot.com',
+  messagingSenderId: '619907598179',
+  appId: '1:619907598179:web:6a9f098b84824dd9d57556',
+  measurementId: 'G-MYEJWNCZVR',
 };
 
 export const firebase = initializeApp(firebaseConfig);
