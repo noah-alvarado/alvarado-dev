6f4620442d516a99a35992a8e6535e0bc53d608e
Noah Alvarado <38107504+noah-alvarado@users.noreply.github.com>
2025-06-02 17:06:16 -0500

    better card

--- src/components/TitleCard.jsx
+++ src/components/TitleCard.jsx
@@ -1,31 +1,36 @@
 import React from 'react';
 
-function TitleCard() {
-  return (
-    <a
-      href="https://alvarado.dev"
-      className="
-        block
-        p-14
-        bg-white dark:bg-gray-800
-        rounded-3xl
-        shadow-[0_8px_40px_0_rgba(0,0,0,0.18)]
-        hover:shadow-[0_12px_48px_0_rgba(0,0,0,0.28)]
-        border border-gray-200 dark:border-gray-700
-        transition-all duration-300
-        text-center
-        cursor-pointer
-      "
-    >
-      <p
-        className="
-          text-gray-600 dark:text-gray-300
-        "
+function TitleCard({ children, href, className = '' }) {
+  const baseClasses = `
+    block
+    p-14
+    bg-white dark:bg-gray-800
+    rounded-3xl
+    shadow-[0_8px_40px_0_rgba(0,0,0,0.18)]
+    hover:shadow-[0_12px_48px_0_rgba(0,0,0,0.28)]
+    border border-gray-200 dark:border-gray-700
+    transition-all duration-300
+    text-center
+    ${className}
+  `;
+  const pointerClass = href ? 'cursor-pointer' : '';
+
+  if (href) {
+    return (
+      <a
+        href={href}
+        className={`${baseClasses} ${pointerClass}`}
       >
-        Click this box to visit my site!
-      </p>
-    </a>
-  );
+        {children}
+      </a>
+    );
+  } else {
+    return (
+      <div className={baseClasses}>
+        {children}
+      </div>
+    );
+  }
 }
 
 export default TitleCard;
--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -19,7 +19,9 @@ export default function Landing() {
             >
                 Welcome to alvarado.dev
             </h1>
-            <TitleCard />
+            <TitleCard href="https://alvarado.dev">
+                Click this box to visit my site!
+            </TitleCard>
         </div>
     );
 }
