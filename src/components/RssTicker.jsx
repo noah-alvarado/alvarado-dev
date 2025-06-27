5c47f709a52bddf2ed6788b0558d1bfe6a6213bf
Noah Alvarado <38107504+noah-alvarado@users.noreply.github.com>
2025-06-27 14:50:14 -0500

    update

--- src/components/RssTicker.jsx
+++ src/components/RssTicker.jsx
@@ -3,12 +3,12 @@ import React from 'react';
 export default function RssTicker() {
     return (
         <iframe
-            src="https://rss.app/embed/v1/ticker/tW38QufZjcn1aSwK"
+            src="https://rss.app/embed/v1/ticker/Ht6VfFXvb11etWpA"
             width="100%"
             height="55"
             frameBorder="0"
             scrolling="no"
-            style={{ border: 'none', overflow: 'hidden', background: 'transparent' }}
+            style={{ border: 'none', overflow: 'hidden', background: 'black' }}
             title="RSS Ticker"
         ></iframe>
     );
