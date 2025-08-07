a62a74bbe09cfc99731106f33b6754c18409dc1a
Noah Alvarado <38107504+noah-alvarado@users.noreply.github.com>
2025-08-07 17:04:41 +0200

    styling fix

--- src/components/PhotoGrid.jsx
+++ src/components/PhotoGrid.jsx
@@ -1,9 +1,21 @@
 import React, { useState } from "react";
 import OptimizedImage from './OptimizedImage.jsx';
 
+function spanToClasses(span) {
+  let classes = '';
+  
+  if (span.x === 1) classes += 'col-span-1 ';
+  else if (span.x === 2) classes += 'col-span-2 ';
+
+  if (span.y === 1) classes += 'row-span-1 ';
+  else if (span.y === 2) classes += 'row-span-2 ';
+
+  return classes.trim();
+}
+
 function photoToGridItem({ src, alt, span: { x, y } }, i) {
   return (
-    <div key={i} className={`col-span-${x} row-span-${y}`}>
+    <div key={i} className={spanToClasses({ x, y })}>
       <OptimizedImage
         basePath={src}
         alt={alt}
